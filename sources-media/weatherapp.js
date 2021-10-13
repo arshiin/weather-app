5f82ee5d756fde6fa7d8535d04c452291939a2ed
arshxn <arshxn@gmail.com>
2021-10-13 22:36:42 +0330

    Added new backgrounds and text colors for each weather condition, added "change background" button group, fixed some bugs

--- sources-media/weatherapp.js
+++ sources-media/weatherapp.js
@@ -25,7 +25,6 @@ function showDate() {
     "December",
   ];
 
-  let currentYear = currentTime.getFullYear();
   let currentMonth = months[currentTime.getMonth()];
   let currentDate = currentTime.getDate();
   let currentDay = days[currentTime.getDay()];
@@ -45,6 +44,46 @@ function showDate() {
 }
 showDate();
 
+function changeTextColors(weatherCondition) {
+  if (weatherCondition == "default") {
+    document.querySelector(".temperature").style.color = "rgb(51, 34, 90)";
+    document.querySelector(".date").style.color = "rgb(165, 133, 226)";
+    document.querySelector(".time").style.color = "rgb(165, 80, 132)";
+    document.querySelector(".feels").style.color = "rgb(78, 150, 124)";
+    document.querySelector(".humidity").style.color = "rgb(75, 149, 192)";
+    document.querySelector(".wind").style.color = "rgb(75, 149, 192)";
+    document.querySelector(".sunrise").style.color = "rgb(255, 136, 61)";
+    document.querySelector(".sunset").style.color = "rgb(75, 69, 112)";
+    document.querySelector(".today").style.boxShadow =
+      "0 8px 32px 0 rgba(116, 117, 134, 0.37)";
+  } else if (weatherCondition == "rain") {
+    document.querySelector(".temperature").style.color = "rgb(255, 255, 255)";
+    document.querySelector(".time").style.color = "rgb(185, 105, 154)";
+    document.querySelector(".feels").style.color = "rgb(174, 212, 85)";
+    document.querySelector(".sunset").style.color = "rgb(179, 167, 248)";
+    document.querySelector(".today").style.boxShadow =
+      "0 8px 32px 0 rgba(183, 184, 204, 0.6)";
+  } else if (weatherCondition == "night") {
+    document.querySelector(".temperature").style.color = "rgb(255, 255, 255)";
+    document.querySelector(".date").style.color = "rgb(194, 176, 228)";
+    document.querySelector(".time").style.color = "rgb(235, 184, 215)";
+    document.querySelector(".sunset").style.color = "rgb(145, 173, 92)";
+    document.querySelector(".today").style.boxShadow =
+      "0 8px 32px 0 rgba(116, 117, 134, 0.37)";
+  } else if (weatherCondition == "thunder") {
+    document.querySelector(".temperature").style.color = "rgb(255, 255, 255)";
+    document.querySelector(".date").style.color = "rgba(66, 24, 145, 0.767)";
+    document.querySelector(".time").style.color = "rgb(165, 80, 132)";
+    document.querySelector(".feels").style.color = "rgb(18, 75, 54)";
+    document.querySelector(".humidity").style.color = "rgb(21, 57, 99)";
+    document.querySelector(".wind").style.color = "rgb(21, 57, 99)";
+    document.querySelector(".sunrise").style.color = "rgb(248, 194, 160)";
+    document.querySelector(".sunset").style.color = "rgb(36, 28, 83)";
+    document.querySelector(".today").style.boxShadow =
+      "0 8px 32px 0 rgba(116, 117, 134, 0.37)";
+  }
+}
+
 function showWeather(response) {
   document.querySelector(".city-name").innerHTML = response.data.name; // city name
   document.querySelector(".current-temp").innerHTML = Math.round(
@@ -55,7 +94,6 @@ function showWeather(response) {
   fahrenheitButton.classList.remove("active");
   fahrenheitButton.removeAttribute("disabled");
   // to go back to default unit (celsius) when a new city is searched
-
   document.querySelector(".max").innerHTML = Math.round(
     response.data.main.temp_max
   ); // max temp
@@ -74,6 +112,9 @@ function showWeather(response) {
   let sunriseTime = new Date(response.data.sys.sunrise * 1000);
   let sunriseHours = sunriseTime.getHours();
   let sunriseMinutes = sunriseTime.getMinutes();
+  if (sunriseHours > 12) {
+    sunriseHours = sunriseHours - 12;
+  }
   if (sunriseHours < 10) {
     sunriseHours = `0${sunriseHours}`;
   }
@@ -103,26 +144,50 @@ function showWeather(response) {
   let description = response.data.weather[0].main;
   condition.innerHTML = description;
   conditionImage.alt = description;
+  let hour = currentTime.getHours();
   if (
     description == "Clear" &&
-    currentTime.getHours() < sunsetTime.getHours()
+    sunriseTime.getHours() < hour &&
+    hour < sunsetTime.getHours()
   ) {
     conditionImage.src = "sources-media/images/sunny.gif";
+    document.body.style.backgroundImage =
+      "url('sources-media/images/clear-bg.jpg')";
+    changeTextColors("default");
   } else if (
-    description == "Clear" &&
-    currentTime.getHours() > sunsetTime.getHours()
+    (description == "Clear" && hour > sunsetTime.getHours()) ||
+    (description == "Clear" && hour > 0 && hour < sunriseTime.getHours())
   ) {
     conditionImage.src = "sources-media/images/clearnight.gif";
+    document.body.style.backgroundImage =
+      "url('sources-media/images/clearnight-bg.jpg')";
+    changeTextColors("night");
   } else if (description == "Clouds") {
     conditionImage.src = "sources-media/images/cloudy.gif";
+    document.body.style.backgroundImage =
+      "url('sources-media/images/cloud-bg.jpg')";
+    changeTextColors("default");
   } else if (description == "Rain" || description == "Drizzle") {
     conditionImage.src = "sources-media/images/rainy.gif";
+    document.body.style.backgroundImage =
+      "url('sources-media/images/rain-bg.jpg')";
+    changeTextColors("rain");
   } else if (description == "Snow") {
     conditionImage.src = "sources-media/images/snowy.gif";
+    document.body.style.backgroundImage =
+      "url('sources-media/images/snow-bg.jpg')";
+    changeTextColors("default");
   } else if (description == "Thunderstorm") {
     conditionImage.src = "sources-media/images/stormy.gif";
+    document.body.style.backgroundImage =
+      "url('sources-media/images/storm-bg.jpg')";
+    changeTextColors("thunder");
+    document.querySelector("#condition").style.fontSize = "34px";
   } else {
     conditionImage.src = "sources-media/images/foggy.gif";
+    document.body.style.backgroundImage =
+      "url('sources-media/images/fog-bg.jpg')";
+    changeTextColors("default");
   }
 }
 
@@ -180,6 +245,44 @@ function detectCity(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(getCoords);
 }
+function changeBg(event) {
+  event.preventDefault();
+  if (event.target.id == "clear-day") {
+    document.body.style.backgroundImage =
+      "url('sources-media/images/clear-bg.jpg')";
+    changeTextColors("default");
+  } else if (event.target.id == "clear-night") {
+    document.body.style.backgroundImage =
+      "url('sources-media/images/clearnight-bg.jpg')";
+    changeTextColors("night");
+  } else if (event.target.id == "clouds") {
+    document.body.style.backgroundImage =
+      "url('sources-media/images/cloud-bg.jpg')";
+    changeTextColors("default");
+  } else if (event.target.id == "rain") {
+    document.body.style.backgroundImage =
+      "url('sources-media/images/rain-bg.jpg')";
+    changeTextColors("rain");
+  } else if (event.target.id == "thunder") {
+    document.body.style.backgroundImage =
+      "url('sources-media/images/storm-bg.jpg')";
+    changeTextColors("thunder");
+  } else if (event.target.id == "snow") {
+    document.body.style.backgroundImage =
+      "url('sources-media/images/snow-bg.jpg')";
+    changeTextColors("default");
+  } else if (event.target.id == "mist") {
+    document.body.style.backgroundImage =
+      "url('sources-media/images/fog-bg.jpg')";
+    changeTextColors("default");
+  }
+}
+
+let buttons = document.querySelectorAll(".btn-secondary");
+buttons.forEach((btn) => {
+  btn.addEventListener("click", changeBg);
+});
+
 let submitCity = document.querySelector("#search-form");
 submitCity.addEventListener("submit", showCity);
 let locationButton = document.querySelector("#location");
